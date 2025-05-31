48c40ac35098104160b248f991f0bff3486da0f5
enoc <enoc.alejandro.castrolobo@gmail.com>
2025-05-30 21:43:02 -0400

    ejecutar después de que todas las operaciones sincrónicas actuales hayan terminado

--- server/server-events/onWebrtcSignal.js
+++ server/server-events/onWebrtcSignal.js
@@ -1,11 +1,17 @@
-let webrtcSignal = (io,socket,data)=>{
-    console.log('webrtc data',socket.id)
-    setTimeout(()=>{
-        if (socket.id == data.ongoingCall.participants.caller) {
-            io.to(data.ongoingCall.participants.reciver).emit('webrtcSignal',data)
-            return
-        }
-        io.to(data.ongoingCall.participants.caller).emit('webrtcSignal',data)
-    },1000)
-} ;
-export default webrtcSignal
+const pasarAlSgtCallStack = new Promise((resolve) => {
+  resolve((io, socket, data) => {
+    if (socket.id === data.ongoingCall.participants.caller) {
+      io.to(data.ongoingCall.participants.reciver).emit('webrtcSignal', data);
+    } else {
+      io.to(data.ongoingCall.participants.caller).emit('webrtcSignal', data);
+    }
+  });
+});
+
+let webrtcSignal = (io, socket, data) => {
+  pasarAlSgtCallStack.then((callback) => {
+    callback(io, socket, data);
+  });
+};
+
+export default webrtcSignal;
